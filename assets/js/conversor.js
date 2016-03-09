094806794d2273b889ea005dffd8634b574b3194
alu0100811933 <alu0100811933@ull.edu.es>
2016-03-09 19:29:14 +0000

    Actualizando funciones

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,12 +1,19 @@
 (function(exports) {
   "use strict";
+  function main() {
+        var valor     = document.getElementById('convert').value,
+            elemento  = document.getElementById('converted');
+        elemento.innerHTML = Medida.convertir(valor);
+        return false;
+    }
+    exports.main = main;
 
   function Medida(valor,tipo) {
+    const expr = XRegExp('(?<num>      [-+]?[^\\.][0-9]+([\\.][0-9]+)?\\s*(?:e[+-]?[ ]*[0-9]+)?)\\s*   # numero       \n' +
+                        '(?<temp1>     [fkcFKC])\\s*                                                   # temperatura1','x');
 
     if (!tipo) {
-      expresion = XRegExp('(?<num>      [-+]?[^\\.][0-9]+([\\.][0-9]+)?\\s*(?:e[+-]?[ ]*[0-9]+)?)\\s*   # numero       \n' +
-                          '(?<temp1>    [fkcFKC])\\s*                                                   # temperatura1','x'),
-      cadena = XRegExp.exec(valor, expresion);
+      cadena = XRegExp.exec(valor, expr);
       this.value = cadena.num;
       this.type = cadena.temp1;
     }
@@ -19,6 +26,65 @@
 
   }
 
+  Medida.prototype.convertir = function(valor){
+  //  var valor     = document.getElementById('convert').value,
+  //      elemento  = document.getElementById('converted'),
+        /* Extienda la RegeExp a la especificación. use una XRegExp */
+        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc])\s*(?:to)?\s*([fkc])$/i,
+
+        expresion = XRegExp('(?<num>   [-+]?[^\\.][0-9]+([\\.][0-9]+)?\\s*(?:e[+-]?[ ]*[0-9]+)?)\\s*   # numero       \n' +
+                            '(?<temp1>    [fkcFKC])\\s*                                       # temperatura1 \n' +
+                            '(?<to>       (?:to)?)\\s*                                        # to           \n' +
+                            '(?<temp2>    [fkcFKC])\\s*                                       # temperatura2','x'),
+
+        valor = XRegExp.exec(valor, expresion);
+
+   if (valor) {
+
+      var numero = valor.num,
+          tipo = valor.temp1.toLowerCase(),
+          tipo2 = valor.temp2.toLowerCase();
+
+      numero = parseFloat(numero);
+      console.log("Valor: " + numero + ", Tipo: " + tipo);
+
+      switch (tipo) {
+        case 'c':
+          var celsius = new Celsius(numero);
+          if (tipo2 == 'f')
+          return celsius.toFarenheit().toFixed(2) + " Farenheit.";
+          if (tipo2 == 'k')
+          return celsius.toKelvin().toFixed(2) + " Kelvin.";
+          if (tipo2 == 'c')
+          return "Conversión inválida";
+          break;
+        case 'f':
+          var farenheit = new Farenheit(numero);
+          if (tipo2 == 'c')
+          return farenheit.toCelsius().toFixed(2) + " Celsius";
+          if (tipo2 == 'k')
+          return farenheit.toKelvin().toFixed(2) + " Kelvin";
+          if (tipo2 == 'f')
+          return "Conversión inválida";
+          break;
+        case 'k':
+          var kelvin = new Kelvin(numero);
+          if (tipo2 == 'c')
+          return kelvin.toCelsius().toFixed(2) + " Celsius";
+          if (tipo2 == 'f')
+          return kelvin.toFarenheit().toFixed(2) + " Farenheit";
+          if (tipo2 == 'k')
+          return "Conversión inválida";
+          break;
+
+        default:
+          elemento.innerHTML = "ERROR! Intenta algo como '-4.2C' ";
+           /* rellene este código */
+      }
+    }
+    else
+      elemento.innerHTML = "Formato de entrada inválido";
+  }
   function Temperatura(valor,tipo) {
     Medida.call(this, valor, tipo);
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
@@ -77,64 +143,6 @@
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
 
-  exports.convertir = function(){
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc])\s*(?:to)?\s*([fkc])$/i,
 
-        expresion = XRegExp('(?<num>   [-+]?[^\\.][0-9]+([\\.][0-9]+)?\\s*(?:e[+-]?[ ]*[0-9]+)?)\\s*   # numero       \n' +
-                            '(?<temp1>    [fkcFKC])\\s*                                       # temperatura1 \n' +
-                            '(?<to>       (?:to)?)\\s*                                        # to           \n' +
-                            '(?<temp2>    [fkcFKC])\\s*                                       # temperatura2','x'),
-
-        valor = XRegExp.exec(valor, expresion);
-
-   if (valor) {
-
-      var numero = valor.num,
-          tipo = valor.temp1.toLowerCase(),
-          tipo2 = valor.temp2.toLowerCase();
-
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
-
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          if (tipo2 == 'f')
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit.";
-          if (tipo2 == 'k')
-          elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin.";
-          if (tipo2 == 'c')
-          elemento.innerHTML = "Conversión inválida";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          if (tipo2 == 'c')
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          if (tipo2 == 'k')
-          elemento.innerHTML = farenheit.toKelvin().toFixed(2) + " Kelvin";
-          if (tipo2 == 'f')
-          elemento.innerHTML = "Conversión inválida";
-          break;
-        case 'k':
-          var kelvin = new Kelvin(numero);
-          if (tipo2 == 'c')
-          elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
-          if (tipo2 == 'f')
-          elemento.innerHTML = kelvin.toFarenheit().toFixed(2) + " Farenheit";
-          if (tipo2 == 'k')
-          elemento.innerHTML = "Conversión inválida";
-          break;
-
-        default:
-          elemento.innerHTML = "ERROR! Intenta algo como '-4.2C' ";
-           /* rellene este código */
-      }
-    }
-    else
-      elemento.innerHTML = "Formato de entrada inválido";
-  }
 
 })(this);
