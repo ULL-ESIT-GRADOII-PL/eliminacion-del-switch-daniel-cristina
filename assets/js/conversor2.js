7e29010316c2453230f0e499ecb93b0627f5419f
alu0100811933 <alu0100811933@ull.edu.es>
2016-03-10 17:34:56 +0000

    Reparando errores de js y eliminando switch smell

--- assets/js/conversor2.js
+++ assets/js/conversor2.js
@@ -1,6 +1,3 @@
-//(function(exports) {
-  //"use strict";
-
 
   function Temperatura(valor,tipo) {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
@@ -61,61 +58,3 @@
   exports.Farenheit = Farenheit;
   exports.Kelvin = Kelvin;
 
-
-/*
-  exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-         Extienda la RegeExp a la especificación. use una XRegExp
-        /*regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([cfk]+)\s*(to)?\s+([cfk])$/i;
-        xregexp = XRegExp('(?<val> [-+]?[0-9]+(\\.[0-9]+)?(?:e[+-]?[0-9]+)?([ ]*)) #val \n' +
-                          '(?<tipo> [cfkCFK]([ ]*)) #tipo \n' +
-                          '(?<to> (to)([ ]+))? #to \n' +
-                          '(?<opt>  [cfkCFK]([ ]*)) #opt','x');
-        valor = XRegExp.exec(valor,xregexp);
-
-    if (valor) {
-      var numero = valor.val,
-      tipo   = valor.tipo.toLowerCase();
-      var  to = valor.opt.toLowerCase();
-
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo + ", Otro: " + to);
-
-      switch (tipo.charAt(0)) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          if(to =='f')
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          if(to =='k')
-            elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
-          if(to =='c')
-            elemento.innerHTML = "Ya esta en Celsius";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          if(to =='c')
-            elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          if(to =='k')
-            elemento.innerHTML = farenheit.toKelvin().toFixed(2) + "Kelvin";
-          if(to =='f')
-            elemento.innerHTML = "Ya esta en farenheint";
-          break;
-        case 'k':
-          var kelvin = new Kelvin(numero);
-          if(to =='c')
-              elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
-          if(to =='f')
-              elemento.innerHTML = kelvin.toFarenheit().toFixed(2) + "Farenheit";
-          if(to =='k')
-              elemento.innerHTML = "Ya esta en Kelvin";
-          break;
-        default:
-          elemento.innerHTML = "NAN";
-      }
-    }
-    else
-      elemento.innerHTML = "Error pruebe a introduciar algo como: 32F C";
-  }
-//})(this);
-*/
